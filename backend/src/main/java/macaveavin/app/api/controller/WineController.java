e07e8bd188105474cdf7bd405f8c612b0f222d8e
lolothi <badboboc69@gmail.com>
2023-10-10 00:06:46 +0200

    crud ok

--- backend/src/main/java/macaveavin/app/api/controller/WineController.java
+++ backend/src/main/java/macaveavin/app/api/controller/WineController.java
@@ -5,6 +5,7 @@ import macaveavin.app.api.service.WineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,13 +19,28 @@ public class WineController {
         return "Hello guys !";
     }
 
+    @GetMapping("/vin")
+    public List<WineDto> geWines() {
+        return  wineService.getWines();
+    }
+
     @GetMapping("/vin/{id}")
     public Optional<WineDto> getOneWine(@PathVariable Long id) {
         return wineService.getWine(id);
     }
 
+    @PatchMapping("vin/{id}")
+    public Optional<WineDto> updateWine(@RequestBody WineDto updatedWineDto, @PathVariable Long id) {
+        return wineService.updateWine(updatedWineDto, id);
+    }
+
     @PostMapping("/vin")
     public WineDto saveWine(@RequestBody WineDto newWineDto) {
         return wineService.saveWine(newWineDto);
+    }
+
+    @DeleteMapping("/vin/{id}")
+    public String deleteWine(@PathVariable Long id) {
+        return wineService.deleteWine(id);
     }
 }
--- backend/src/main/java/macaveavin/app/api/service/WineService.java
+++ backend/src/main/java/macaveavin/app/api/service/WineService.java
@@ -7,7 +7,9 @@ import macaveavin.app.api.service.mapper.WineMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class WineService {
@@ -18,27 +20,41 @@ public class WineService {
     @Autowired
     private WineMapper wineMapper;
 
-    public WineService() {
-    }
 
-    /*
     public List<WineDto> getWines() {
         return ((List<Wine>) wineRepository.findAll()).stream()
                 .map(wine -> wineMapper.convertToDto(Optional.ofNullable(wine)))
                 .collect(Collectors.toList());
-    } */
+    }
 
     public Optional<WineDto> getWine(Long id){
         Optional<Wine> wine = wineRepository.findById(id);
-        System.out.println("-----wine: "+ wine);
         if (wine.isPresent()) {
             return Optional.ofNullable(wineMapper.convertToDto(wine));
         }
         return Optional.empty();
     }
 
+    public Optional<WineDto> updateWine(WineDto updatedWineDto, Long id){
+        Wine wine = wineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Erreur id:" + id));
+        wine.setName(updatedWineDto.getName());
+        wine.setYear(updatedWineDto.getYear());
+        wine.setType(updatedWineDto.getType());
+        wine.setCountry(updatedWineDto.getCountry());
+        wine.setEan(updatedWineDto.getEan());
+        wine.setComments(updatedWineDto.getComments());
+        wineRepository.save(wine);
+        return Optional.ofNullable(wineMapper.convertToDto(Optional.of(wine)));
+    }
+
     public WineDto saveWine(WineDto wineDto) {
         wineRepository.save(wineMapper.convertToEntity(wineDto));
         return wineDto;
+    }
+
+    public String deleteWine(Long id) {
+        Wine wine = wineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Erreur id:" + id));
+        wineRepository.delete(wine);
+        return "Vin supprimé";
     }
 }
