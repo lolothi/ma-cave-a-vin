d9683ad4d75ec1e616a1181d62185dca3e5c304a
Laurent <laurent@bigdell.home>
2023-11-28 16:06:02 +0100

    updates for front

--- backend/src/main/java/macaveavin/app/api/controller/WineController.java
+++ backend/src/main/java/macaveavin/app/api/controller/WineController.java
@@ -1,12 +1,10 @@
 package macaveavin.app.api.controller;
 
 import macaveavin.app.api.dto.WineDto;
-import macaveavin.app.api.entity.WineTypeEnum;
 import macaveavin.app.api.service.WineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,37 +22,35 @@ public class WineController {
 
     @GetMapping("/wine")
     public List<WineDto> geWines() {
-        //return pour les tests avec le front end
-        List<WineDto> wineList = new ArrayList<>();
+//        List<WineDto> wineList = new ArrayList<>();
+//
+//        wineList.add(new WineDto(1L, "Château Margaux", 2010, WineTypeEnum.RED, "France",
+//                "Château Margaux", "Cabernet Sauvignon", "Grand Cru Classé", "1234567890123",
+//                "image1.jpg", "Grilled meats", 95, "A classic Bordeaux wine", "Premium",
+//                100, 50));
+//
+//        wineList.add(new WineDto(2L, "Barolo Riserva", 2012, WineTypeEnum.RED, "Italy", "Ceretto",
+//                "Nebbiolo", "Riserva", "2345678901234", "image2.jpg",
+//                "Pasta with truffle sauce", 93, "A rich and robust Barolo", "Reserve", 75, 25));
+//
+//        wineList.add(new WineDto(3L, "Riesling Spätlese", 2015, WineTypeEnum.WHITE, "Germany",
+//                "Dr. Loosen", "Riesling", "Spätlese", "3456789012345", "image3.jpg",
+//                "Spicy Thai cuisine", 88, "A sweet and aromatic Riesling", "Sweet", 50, 20));
+//
+//        return wineList;
 
-        wineList.add(new WineDto(1L, "Château Margaux", 2010, WineTypeEnum.RED, "France",
-                "Château Margaux", "Cabernet Sauvignon", "Grand Cru Classé", "1234567890123",
-                "image1.jpg", "Grilled meats", 95, "A classic Bordeaux wine", "Premium",
-                100, 50));
-
-        wineList.add(new WineDto(2L, "Barolo Riserva", 2012, WineTypeEnum.RED, "Italy", "Ceretto",
-                "Nebbiolo", "Riserva", "2345678901234", "image2.jpg",
-                "Pasta with truffle sauce", 93, "A rich and robust Barolo", "Reserve", 75, 25));
-
-        wineList.add(new WineDto(3L, "Riesling Spätlese", 2015, WineTypeEnum.WHITE, "Germany",
-                "Dr. Loosen", "Riesling", "Spätlese", "3456789012345", "image3.jpg",
-                "Spicy Thai cuisine", 88, "A sweet and aromatic Riesling", "Sweet", 50, 20));
-
-        return wineList;
-
-        //return  wineService.getWines();
+        return  wineService.getWines();
     }
 
 
     @GetMapping("/wine/{id}")
     public Optional<WineDto> getOneWine(@PathVariable Long id) {
-        //return pour les tests avec le front end
-        Optional<WineDto> optionalWineDto = Optional.of(new WineDto(1L, "Château Margaux", 2010, WineTypeEnum.RED, "France",
-                "Château Margaux", "Cabernet Sauvignon", "Grand Cru Classé", "1234567890123",
-                "image1.jpg", "Grilled meats", 95, "A classic Bordeaux wine", "Premium",
-                100, 50));
-        return optionalWineDto;
-        //return wineService.getWine(id);
+//        Optional<WineDto> optionalWineDto = Optional.of(new WineDto(1L, "Château Margaux", 2010, WineTypeEnum.RED, "France",
+//                "Château Margaux", "Cabernet Sauvignon", "Grand Cru Classé", "1234567890123",
+//                "image1.jpg", "Grilled meats", 95, "A classic Bordeaux wine", "Premium",
+//                100, 50));
+//        return optionalWineDto;
+        return wineService.getWine(id);
     }
 
     @PatchMapping("wine/{id}")
